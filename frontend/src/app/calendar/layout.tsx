2f7a841eaa1124704e00ee51764b118c91e817fc
Piotr Marcol <52676616+p-marcol@users.noreply.github.com>
2023-12-12 23:53:18 +0100

    SideNav v0.5

--- frontend/src/app/calendar/layout.tsx
+++ frontend/src/app/calendar/layout.tsx
@@ -1,9 +1,13 @@
 "use client";
 
-import SideNav from "@/ui/SideNav";
+import { SideNav } from "@/ui/SideNav";
 import DailyPlanner from "@/ui/DailyPlanner";
 
-export default function CalendarLayout({ children }: { children: React.ReactNode }) {
+export default function CalendarLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
 	return (
 		<div className="flex w-full h-screen">
 			<SideNav />
