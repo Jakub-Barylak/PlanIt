42eddd222adf2570df4b658b46b6ad8648f483d0
Piotr Marcol <52676616+p-marcol@users.noreply.github.com>
2023-12-19 23:34:03 +0100

    show calendars from server

--- frontend/src/ui/Sidebar/SidebarCalendarView.tsx
+++ frontend/src/ui/Sidebar/SidebarCalendarView.tsx
@@ -1,40 +1,71 @@
-import Image from "next/image";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { AuthContextType, AuthContext } from "@/providers/AuthProvider";
 
-export default function CalendarView() {
-	let calendars = ["Calendar 1", "Calendar 2"];
+import type { Calendar } from "@/lib/types";
+
+export default function CalendarView({ calendars }: { calendars: Calendar[] }) {
+	// let calendars = ["Calendar 1", "Calendar 2"];
+
+	const [newCalendarName, setNewCalendarName] = useState<string>("");
+	const { axios } = useContext(AuthContext) as AuthContextType;
+
+	let addCalendar = async (e: React.FormEvent) => {
+		e.preventDefault();
+
+		axios
+			.post("/user_calendars/", {
+				name: newCalendarName,
+				color: "#000000",
+			})
+			.then((response) => {
+				console.log(response);
+				// calendars.push(response.data as Calendar);
+				setNewCalendarName("");
+			})
+			.catch((error) => {
+				alert("Error creating calendar!");
+				console.log(error);
+			});
+	};
 
 	return (
 		<>
 			{calendars.map((calendar, index) => {
 				const [isChecked, setIsChecked] = useState(
-					sessionStorage.getItem(calendar) === "true",
+					sessionStorage.getItem(calendar.name) !== "true",
 				);
-				const onChange = (key: string, value: boolean) => {
+
+				const changeChecked = (key: string, value: boolean) => {
 					sessionStorage.setItem(key, value.toString());
 					setIsChecked(value);
 				};
+
 				return (
 					<label
 						key={index}
 						className="flex h-8 items-center justify-between rounded-lg px-2 hover:bg-gray-100 dark:hover:bg-[#282A32]"
 					>
 						{/* grid grid-cols-[max-content_auto] */}
-						<span>{calendar}</span>
+						<span>{calendar.name}</span>
 						<input
 							type="checkbox"
 							className="ml-2 h-4 w-4 bg-gray-200 text-gray-600 accent-slate-600"
 							checked={isChecked}
-							onChange={(e) => onChange(calendar, e.target.checked)}
+							onChange={(e) => changeChecked(calendar.name, e.target.checked)}
 						/>
 					</label>
 				);
 			})}
-			<div className="flex items-center justify-between rounded-lg p-2">
+			<form
+				className="flex items-center justify-between rounded-lg p-2"
+				onSubmit={addCalendar}
+			>
 				<input
 					type="text"
 					className="h-8 self-stretch rounded-lg border border-gray-300 px-2 dark:bg-[#4A4D54]"
 					placeholder="New calendar"
+					value={newCalendarName}
+					onChange={(e) => setNewCalendarName(e.target.value)}
 				/>
 				{/* PLUS BUTTON */}
 				<div className="ml-2 h-4 w-4 cursor-pointer overflow-hidden rounded border-2 border-black bg-white dark:border-white dark:bg-black dark:text-white">
@@ -42,7 +73,7 @@ export default function CalendarView() {
 						+
 					</div>
 				</div>
-			</div>
+			</form>
 		</>
 	);
 }
