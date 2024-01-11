39f1d7022d0014adaf9287b21527e0fa889c2141
Piotr Marcol <52676616+p-marcol@users.noreply.github.com>
2024-01-11 22:12:43 +0100

    Remove console.log statement in EventView.tsx

--- frontend/src/ui/calendar/views/monthView/EventView.tsx
+++ frontend/src/ui/calendar/views/monthView/EventView.tsx
@@ -16,7 +16,6 @@ export default function EventView(props: EventViewProps) {
 			const r = parseInt(color.slice(1, 3), 16);
 			const g = parseInt(color.slice(3, 5), 16);
 			const b = parseInt(color.slice(5, 7), 16);
-			console.log(r, g, b);
 			const brightness = (r * 299 + g * 587 + b * 114) / 1000;
 			return brightness >= 128 ? "#000000" : "#ffffff";
 		};
